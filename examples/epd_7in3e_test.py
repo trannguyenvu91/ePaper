146b17f9ef88ec31f3276d05c6678609864d8b06
Vu Tran <trannguyenvu91@gmail.com>
2025-12-02 14:43:31 +0700

    Update test

--- examples/epd_7in3e_test.py
+++ examples/epd_7in3e_test.py
@@ -24,7 +24,7 @@ try:
 
     # read bmp file 
     logging.info("2.read bmp file")
-    Himage = Image.open(os.path.join(picdir, 'test.bmp'))
+    Himage = Image.open(os.path.join(picdir, 'test2.bmp'))
     epd.display(epd.getbuffer(Himage))
     time.sleep(3)
     
